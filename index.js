228e9b35efda39767ae6454130ff17e7278c4f27
Abby Anderson <abby.e.anderson@gmail.com>
2021-08-31 11:10:06 -0600

    commit tree de42f29ee82b99b89043eea24dba285294d47d2c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+        }
+
+
+//should be able to just delete this try that's below, the above should be passing the lab! need to check tho
+//    const innerFunction = function (adjective = "special") {
+//         return `You are ${flair}${adjective}${flair}`;
+//     }
+//     return innerFunction(adjective);
+}
+
+
+
+//another example from talking thorugh with dex
+//user orders drink first, then later orders their food 
+
+// let orderMeal = (drink = "water") => {
+//     return function(food = "bread") {
+//         return `Here is your ${drink} and your ${food}!`
+//     }
+// }
